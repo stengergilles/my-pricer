a2b7468cea9365c679920cfd7303eb2c09844e34
Gilles Stenger <gilles@gillesstenger.fr>
2025-06-03 07:28:44 +0200

    backup

--- src/platform/android/keyboard_helper.h
+++ src/platform/android/keyboard_helper.h
@@ -7,8 +7,14 @@ extern "C" {
 // Function to show the keyboard from native code
 void showKeyboard();
 
+// Function to hide the keyboard from native code
+void hideKeyboard();
+
 // Safer version that returns success/failure
 bool showKeyboardSafely();
+
+// Safer version that returns success/failure
+bool hideKeyboardSafely();
 
 #ifdef __cplusplus
 }
