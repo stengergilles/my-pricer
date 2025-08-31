b049f44189149ab28f5815fd6c660d9b116d9e25
Gilles Stenger <gilles@gillesstenger.fr>
2025-08-31 14:04:56 +0200

    fix(frontend): Persist Auth0 session in local storage

--- web/frontend/src/index.js
+++ web/frontend/src/index.js
@@ -43,6 +43,7 @@ root.render(
         <Auth0Provider
           domain={process.env.REACT_APP_AUTH0_DOMAIN}
           clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+          cacheLocation="localstorage"
           authorizationParams={{
             redirect_uri: window.location.origin,
             audience: process.env.REACT_APP_AUTH0_AUDIENCE,
